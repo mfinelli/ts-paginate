d1cbbdadb372dd41b79fb094b2697df17894085a
Mario Finelli <mario@finel.li>
2024-07-09 18:37:21 -0400

    Use default pagy implementation

--- gentests.rb
+++ gentests.rb
@@ -5,7 +5,7 @@ require 'pagy'
 
 (1..10).each do |p|
   (p..10).each do |c|
-    s = Pagy.new(count: c, items: 1, page: p, size: [1, 1, 1, 1]).series
+    s = Pagy.new(count: c, items: 1, page: p).series
     puts "expect(paginate(#{c}, #{p})).toStrictEqual(#{s.map do |v|
       if v == :gap
         '...'

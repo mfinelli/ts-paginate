e33a1bff5c00c1fe7ae85dd06ca481c0dc9ccc46
Mario Finelli <mario@finel.li>
2024-07-09 18:41:16 -0400

    Add initial paginate implementation

--- index.ts
+++ index.ts
@@ -1,3 +1,51 @@
-export function paginate() {
-  return 1 + 1;
+export type Gap = "...";
+
+export function paginate(pages: number, current: number): Array<number | Gap> {
+  const size = 7;
+
+  if (size >= pages) {
+    const view: Array<number> = [];
+    for (let i = 1; i <= pages; i++) {
+      view.push(i);
+    }
+    return view;
+  }
+
+  const view: Array<number | Gap> = [];
+  const left = Math.floor((size - 1) / 2.0);
+  let start: number;
+
+  if (current <= left) {
+    start = 1;
+  } else if (current > pages - size + left) {
+    start = pages - size + 1;
+  } else {
+    start = current - left;
+  }
+
+  for (let i = start; i < start + size; i++) {
+    view.push(i);
+  }
+
+  if (view[0] !== 1) {
+    view[0] = 1;
+  }
+
+  if (view[1] !== 2) {
+    view[1] = "...";
+  }
+
+  if (view[1] !== 2) {
+    view[1] = "...";
+  }
+
+  if (view[view.length - 2] !== pages - 1) {
+    view[view.length - 2] = "...";
+  }
+
+  if (view[view.length - 1] !== pages) {
+    view[view.length - 1] = pages;
+  }
+
+  return view;
 }
